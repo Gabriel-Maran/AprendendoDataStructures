3cd83261b3c9bf6822808ed295a2d778ea3cbf7d
GabrielMaran <gabriel.enzo.libero.maran@gmail.com>
2025-05-12 10:18:16 -0300

    Initial commit

---
+++ src/AprendendoQueue/QueueTeste01.java
@@ -0,0 +1,25 @@
+package AprendendoQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTeste01 {
+
+    // add = enqueue, offer()
+    // remove = dequeue, poll()
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        System.out.println("Tamanho: " + queue.size());
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        System.out.println("Tamanho: " + queue.size());
+        System.out.println("Contem o numero 30? " + queue.contains(30));
+        System.out.println("Fila: " + queue);
+        System.out.println("Primeiro elemento da fila: " + queue.peek());// Pega, sem remover, o primeiro elemento
+        System.out.println("Item removido: " + queue.poll());// Remove o primeiro elemento da fila
+        System.out.println("Fila: " + queue);
+
+
+    }
+}
---
+++ src/AprendendoStack/StackTeste01.java
@@ -0,0 +1,23 @@
+package AprendendoStack;
+
+import java.util.Stack;
+
+public class StackTeste01 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Elemento no topo: " + stack.peek());
+        System.out.println("Stack: " + stack);
+        System.out.println("Elemento removido: " + stack.pop());  //Remove o topo da lista(Ultimo elemento adc)
+        System.out.println("Elemento no topo: " + stack.peek()); //Pega sem remover o ultimo elemento
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Posição do número '10': " + stack.search(10));
+        System.out.println("Posição do número '100'(Não contem no Stack): " + stack.search(100));//Search retorna -1 caso não esteja presente
+
+
+    }
+}
